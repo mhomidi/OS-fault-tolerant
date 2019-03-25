24a58ccadb94c70f5fc158a8eee9ebb22d6f7c95
mhomidi <mohammadhadi.omidi95@gmail.com>
2019-03-25 21:25:08 +0430

    prepare main.cpp

--- main.cpp
+++ main.cpp
@@ -7,7 +7,28 @@
 int main() {
     // filesFuncTest();
 
-    workerDoTest();
+//    workerDoTest();
+
+
+
+    vector<pid_t> pids;
+    vector<string> fileDirs = files();
+    int numberOfChildren = fileDirs.size();
+
+
+    for (int k = 0; k < numberOfChildren; ++k) {
+        pids.push_back(0);
+    }
+
+    for (int i = 0; i < numberOfChildren; ++i) {
+        if ((pids[i] = fork()) < 0) {
+            perror("fork");
+            abort();
+        } else if (pids[i] == 0) {
+            cout << "I am child" << endl;
+            exit(0);
+        }
+    }
 
     return 0;
 }
